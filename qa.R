22d2e4de26a73e1be3cd32846067a0a6005a3e78
Sergei Matusevich <sergei.matusevich@gmail.com>
2011-05-31 12:34:48 -0700

    first working verison of qa.R - still have to figure out the ordering

--- qa.R
+++ qa.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 
 dd <- read.csv("data/2011_may_final.csv")
 
@@ -7,26 +8,28 @@ qa <- dd[dd$cls == "QA", c("salary", "loc", "exp",
 
 qa <- qa[!qa$salary > mean(qa$salary)+2*sd(qa$salary),]
 
+qa$salaryJitter <- qa$salary  + runif(length(qa$salary ),    50,   50)
+qa$expJitter    <- qa$exp     + runif(length(qa$exp    ), -0.25, 0.25)
+qa$ageJitter    <- qa$Возраст + runif(length(qa$Возраст), -0.25, 0.25)
+
 ################################################################
 
 library(lattice)
 
-png("reports/qa.%03d.png", width=1024, height=480, res=100)
+png(filename="reports/may2011/qa.%03d.png",
+    width=1024, height=320, res=100)
 
-xyplot(I(salary  + runif(length(qa$salary ), -20,   20  ))
-     ~ I(Возраст + runif(length(qa$Возраст), -0.25, 0.25))
-     | loc, data=qa,
-       panel=function(x,y) {
-         panel.xyplot(x,y);
-         panel.loess(x,y,col="brown");
-       }, xlab="Возраст, лет", ylab="Зарплата, $/мес")
+panel.scatter.loess <- function(x,y) {
+  panel.xyplot(x, y);
+  panel.loess(x, y, col="brown");
+}
 
-xyplot(I(salary + runif(length(qa$salary), -20,   20  ))
-     ~ I(exp    + runif(length(qa$exp   ), -0.25, 0.25))
-     | loc, data=qa,
-       panel=function(x,y) {
-         panel.xyplot(x,y);
-         panel.loess(x,y,col="brown");
-       }, xlab="Опыт работы, лет", ylab="Зарплата, $/мес")
+xyplot(salaryJitter ~ expJitter | loc, data=qa,
+       layout=c(5,1), panel=panel.scatter.loess,
+       xlab="Опыт работы, лет", ylab="Зарплата, $/мес")
+
+xyplot(salaryJitter ~ ageJitter | loc, data=qa,
+       layout=c(5,1), panel=panel.scatter.loess,
+       xlab="Возраст, лет", ylab="Зарплата, $/мес")
 
 dev.off()
