3ad2b6f2fcfa964430aeebb677fa2db24dee488c
Sergei Matusevich <sergei.matusevich@gmail.com>
2011-06-01 13:03:32 -0700

    figure out the browser in toolset.R; make browser and platform regex logic more generic

--- toolset.R
+++ toolset.R
@@ -2,20 +2,29 @@
 
 source("read.salary.R")
 
+assignCategory <- function(dd, dst, agent, lvl, regex) {
+  dd[,dst] <- factor(NA, levels=lvl)
+  for (i in 1:length(regex)) {
+    dd[grepl(regex[i], agent, perl=T), dst] <- lvl[i]
+  }
+  return (dd)
+}
+
+################################################################
+
 dd <- read.salary("data/2011_may_final.csv")
 
 dd$User.Agent <- as.character(dd$User.Agent)
 
-dd$platform <- factor(NA,
-  levels=c("Windows", "Linux", "Mac", "Android", "iPhone/iPad"))
+dd <- assignCategory(dd, "platform", dd$User.Agent,
+  c("Windows", "Linux", "Mac", "Android", "iPhone/iPad"),
+  c("Windows NT|WOW64", "(?!Android)Linux", "Macintosh",
+    "Android", "like Mac OS X"))
 
-regexPlatform <- c("Windows NT", "(?!Android)Linux",
-                   "Macintosh", "Android", "like Mac OS X")
-
-for (i in 1:length(regexPlatform)) {
-  dd$platform[grepl(
-    regexPlatform[i], dd$User.Agent, perl=T)] <- levels(dd$platform)[i]
-}
+dd <- assignCategory(dd, "browser", dd$User.Agent,
+  c("MSIE", "Firefox", "Chrome", "Opera", "Safari", "Mobile"),
+  c(" MSIE ", " Firefox/", " Chrome/", "Opera/",
+    " Version/[0-9.]* Safari", " Mobile"))
 
 ################################################################
 
