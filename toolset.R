1db9c334ab975b4597a8ba5afb0b07c931fd4eb1
Sergei Matusevich <sergei.matusevich@gmail.com>
2011-05-31 22:43:33 -0700

    figure out the user platform correctly; no browser or plotting functionality yet

--- toolset.R
+++ toolset.R
@@ -4,17 +4,27 @@ source("read.salary.R")
 
 dd <- read.salary("data/2011_may_final.csv")
 
-dd$User.Agent[grep("Android|Macintosh|Linux|like Mac OS X", dd$User.Agent, invert=T)]
+dd$User.Agent <- as.character(dd$User.Agent)
 
+dd$platform <- factor(NA,
+  levels=c("Windows", "Linux", "Mac", "Android", "iPhone/iPad"))
+
+regexPlatform <- c("Windows NT", "(?!Android)Linux",
+                   "Macintosh", "Android", "like Mac OS X")
+
+for (i in 1:length(regexPlatform)) {
+  dd$platform[grepl(
+    regexPlatform[i], dd$User.Agent, perl=T)] <- levels(dd$platform)[i]
+}
 
 ################################################################
 
 library(lattice)
 
-png(filename="reports/may2011/salary.%03d.png",
-    width=1024, height=1024, res=90)
+# png(filename="reports/may2011/toolset.%03d.png",
+#     width=1024, height=1024, res=90)
 
 
 
-invisible(dev.off())
+# invisible(dev.off())
 
