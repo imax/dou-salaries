37b27cc93f1803b415dc568b71063332334a2283
hash3g <hash.3g@gmail.com>
2013-06-18 13:23:48 +0300

    new data for may2013

--- clean_data.R
+++ clean_data.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 
 clean.salary.csv <- function(infile, outfile) {
 
@@ -5,8 +6,11 @@ dd <- read.csv(infile)
 
 # чтобы убрать factors сначала преобразуем в строки
 dd$exp <- as.numeric(as.character(dd$Общий.опыт.работы))
+dd$current_job_exp <- as.numeric(as.character(dd$Опыт.работы.на.текущем.месте))
 dd[dd$Общий.опыт.работы == "10 и более лет",c("exp")] <- 10
 dd[dd$Общий.опыт.работы == "меньше 3 месяцев",c("exp")] <- 0
+dd[dd$Опыт.работы.на.текущем.месте == "10 и более лет",c("current_job_exp")] <- 10
+dd[dd$Опыт.работы.на.текущем.месте == "меньше 3 месяцев",c("current_job_exp")] <- 0
 
 # сокращаем "Днепропетровск" до "Днепр.", для графиков
 dd$Город <- as.character(dd$Город)
@@ -18,27 +22,28 @@ top_cities <- c("Киев", "Харьков", "Львов", "Днепр.", "othe
 dd$loc <- sapply(dd$Город, function(city) { factor(if (city %in% top_cities) substr(city, 1, 9) else "other", levels=top_cities) })
 
 # переводим все зарплаты в доллары
-dd$salary <- dd$Средняя.зарплата.в.месяц
+dd$salary <- 900
+# dd$Зарплата.в.месяц
 
 # пытаемся убрать ошибки пользователей с неправильной валютой
 dd[dd$salary > 5000 & dd$Возраст<26,c("Валюта")] <- "h"
-dd[dd$Средняя.зарплата.в.месяц <2500 & dd$Валюта == "h",c("Валюта")] <- "d"
+# dd[dd$Зарплата.в.месяц <2500 & dd$Валюта == "h",c("Валюта")] <- "d"
 
 # dd[dd$salary > 4000,c("Валюта", "salary", "exp", "loc", "title")]
-dd$salary[dd$Валюта == "h"] <- dd$Средняя.зарплата.в.месяц[dd$Валюта == "h"] / 8.0
-# убираем подозрительные анкеты с зарплатами меньше $150 
+# dd$salary[dd$Валюта == "h"] <- dd$Зарплата.в.месяц[dd$Валюта == "h"] / 8.0
+# убираем подозрительные анкеты с зарплатами меньше $150
 dd <- dd[!dd$salary<250,]
 dd <- dd[!dd$salary>7000,]
 
 pm_titles = c("Team lead", "Project manager")
-spm_titles = c( "Senior Project Manager / Program Manager", 	
+spm_titles = c( "Senior Project Manager / Program Manager",
 	"Director of Engineering / Program Director")
 dev_titles = c("Junior Software Engineer", "Software Engineer",
 	"Senior Software Engineer")
 sdev_titles = c("Technical Lead", "System Architect")
 qa_titles = c("Junior QA engineer", "QA engineer",
 	"Senior QA engineer", "QA Tech Lead")
-adm_titles = c("DBA / Администратор баз данных", "Системный администратор") 
+adm_titles = c("DBA / Администратор баз данных", "Системный администратор")
 other_titles = c("Верстальщик", "Гейм-дизайнер", "Дизайнер", "Технический писатель")
 
 
@@ -52,17 +57,22 @@ dd[dd$Должность %in% qa_titles, c("cls")] <- "QA"
 dd[dd$Должность %in% adm_titles, c("cls")] <- "ADM"
 dd$cls <- factor(dd$cls)
 
-dd$Возраст[dd$Возраст<15] <- NA
-dd$Возраст[dd$Возраст>65] <- NA
+dd$Возраст[dd$Возраст<15] <- 0
+dd$Возраст[dd$Возраст>65] <- 0
+dd$Возраст[!dd$Возраст] <- 0
+
+dd$Пол <- as.character(dd$Пол)
+dd[dd$Пол == "m", c("Пол")] <- "мужской"
+dd[dd$Пол == "f", c("Пол")] <- "женский"
 
 write.table(dd, file=outfile, sep=",")
-dd_mini <- dd[,c("Город", "salary", "Должность", "exp", "Язык.программирования")]
+dd_mini <- dd[,c("Город", "Зарплата.в.месяц", "Изменение.зарплаты", "Должность", "exp", "current_job_exp", "Язык.программирования", "Специализация", "Возраст", "Пол", "Образование", "Уровень.английского", "Размер.компании")]
 minifile <- sub("final", "mini", outfile)
 write.table(dd_mini, file=minifile, sep=",")
 }
 
 #outfile <- sub("raw", "final", infile)
-source <- "data/2012_dec_raw.csv"
+source <- "data/2013_may_raw.csv"
 final <- sub("raw", "final", source)
 mini <- sub("raw", "mini", source)
 
